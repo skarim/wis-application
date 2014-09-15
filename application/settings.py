7c69b3c454525fde6e9c65080dc916e994741ff0
Sameen Karim <sameenkarim@gmail.com>
2014-09-14 17:56:37 -0700

    fixing base dir in settings.py

--- application/settings.py
+++ application/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from mongoengine import *
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+#BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 
 
 DEBUG = True
