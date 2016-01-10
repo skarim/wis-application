a44e52921bb8a45b5d8a0072182c04a8ec1b3be3
Sameen Karim <sameenkarim@gmail.com>
2016-01-10 14:35:07 -0500

    always send volunteer invite emails
    unless that person has already created an account

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -11,30 +11,29 @@ def import_volunteer(email, first_name, last_name):
     success, error = ('',)*2
     if email and first_name and last_name:
         validate_email(email)
-        # check to make sure user is new and unique
-        unique_user = True
-        try:
-            Allowed_User.objects.get(email=email)
-            unique_user = False
-        except DoesNotExist:
-            pass
+        # check to make sure user does not already have an account
+        user_exists = False
         try:
             WIS_User.objects.get(email=email)
-            unique_user = False
+            user_exists = True
         except DoesNotExist:
             pass
 
-        if unique_user:
-            new_user = Allowed_User()
-            new_user.email = email
-            new_user.first_name = first_name
-            new_user.last_name = last_name
-            new_user.save()
+        if not user_exists:
+            try:
+                # send email even if already been invited previously
+                new_user = Allowed_User.objects.get(email=email)
+            except DoesNotExist:
+                new_user = Allowed_User()
+                new_user.email = email
+                new_user.first_name = first_name
+                new_user.last_name = last_name
+                new_user.save()
             send_welcome_email(new_user)
             success = 'Sent email to %s for account activation'\
                       % new_user.email
         else:
-            error = 'A volunteer with that email already exists'
+            error = 'An account with that email already exists'
     else:
         error = 'Please fill out all fields'
     return (success, error,)
